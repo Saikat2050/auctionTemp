d39e0aa3d37d23460a9d8c1e090ddc1f9dadcb48
saumadipta21 <saumadipta.maity@gmail.com>
2022-10-23 09:52:10 +0530

    auth change

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -8,13 +8,17 @@ const notAuth = process.env.NOTAUTH;
 const seller_dashboard = async(req,res)=>{
     try{
         const authUser = (req.cookies.authToken);
-        const decoded = await jwt.verify(authUser, secret);
+        if(!authUser)
+            res.status(400).send(notAuth);
+        else{
+            const decoded = await jwt.verify(authUser, secret);
         if(!decoded)
             res.status(401).send(notAuth);
         else{
             const result =await Seller.findById(decoded.data);
             res.status(200).send(result.email);
         }
+        } 
     }
     catch(err){
         res.status(400).send(err);
